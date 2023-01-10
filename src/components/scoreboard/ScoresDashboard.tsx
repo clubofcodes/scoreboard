d1e43f1f36c3ff904ead3abe24bcd03f90fd4b5a
Rahul Jagetia <rahulj.inexture@gmail.com>
2023-01-10 11:33:33 +0000

    Listing of streamers JSON data and designing list component

--- src/components/scoreboard/ScoresDashboard.tsx
+++ src/components/scoreboard/ScoresDashboard.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+// React library and hooks API
+import React, { useEffect, useState } from "react";
+
+// styled-component library for designing components
 import styled from "styled-components";
 
-const ScoresDashboard = () => {
-  const Box = styled.div`
-    color: ${(props) => props.theme.color};
-    display: flex;
-  `;
+// JSON data
+import streamersData from "assets/data/streamers.json";
 
-  return <Box theme={{ color: "#000" }}>ScoresDashboard</Box>;
+// Utils
+import { StreamersType } from "utils/globalTypes";
+import { isEmpty } from "utils/constants";
+
+// Custom component to list the steamers
+import PlayersList from "components/playerslist/PlayersList";
+
+// -------------------- Custom Style using styled-component --------------------
+const DashboardContainer = styled.div`
+    border-radius: 10px;
+    background-color: white;
+    padding: 5px 20px;
+  `;
+// -------------------- Custom Style using styled-component --------------------
+
+// JSX function starts from here
+const ScoresDashboard = () => {
+
+  // React state to store streamers data
+  const [streamersList, setStreamersList] = useState<StreamersType[]>([]);
+
+  // To set streamersData in react state and to avoid side-effects
+  useEffect(() => {
+    if (!isEmpty(streamersData)) setStreamersList(streamersData);
+  }, [streamersData]);
+
+  return (
+    <DashboardContainer>
+      <PlayersList data={streamersList} />
+    </DashboardContainer>
+  );
 };
 
 export default ScoresDashboard;
